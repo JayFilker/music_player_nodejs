e89ca82ed0cf2b03952fa9945036487814a10183
18050939892 <deerkesi3815@gmail.com>
2025-07-06 20:38:13 +0800

    Merge remote-tracking branch 'origin/master'

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cors({
   origin: '*',  // 允许所有来源
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],  // 添加 OPTIONS
-  credentials: false
+  allowedHeaders: '*'
 }));
 
 // 2. 添加简单的请求日志和手动 CORS 头 - 帮助调试
@@ -332,7 +332,7 @@ app.get('/api/videos/info', (req, res) => {
 })
 
 const MongDBUrl='mongodb+srv://18050939892:deerkesi3815@blog.ssrtblo.mongodb.net/blogBatabase?retryWrites=true&w=majority&appName=blog'
-app.post('/addLikeSong', async (req, res) => {
+app.post('/add-like-song', async (req, res) => {
   const MONGODB_URI = MongDBUrl;
   const newComment = req.body;
   const client = new MongoClient(MONGODB_URI);
@@ -351,7 +351,7 @@ app.post('/addLikeSong', async (req, res) => {
   });
 });
 
-app.post('/removeLikeSong', async (req, res) => {
+app.post('/remove-like-song', async (req, res) => {
   const MONGODB_URI = MongDBUrl;
   const songData = req.body;
   const client = new MongoClient(MONGODB_URI);
@@ -387,7 +387,7 @@ app.post('/removeLikeSong', async (req, res) => {
 });
 
 
-app.get('/mySongs', async (req, res) => {
+app.get('/my-songs', async (req, res) => {
   const client = new MongoClient(MongDBUrl);
   await client.connect();
   const database = client.db("music-player");
